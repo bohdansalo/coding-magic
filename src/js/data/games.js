0d39e37d4f9e4c7b160854bc6c49f6f6a682605e
danylyoenko <lockmaster19@gmail.com>
2025-05-31 18:34:17 +0300

    cmplt render and game guess the number

---
+++ src/js/data/games.js
@@ -0,0 +1,67 @@
+import { guessNumberInit } from '../games/guess-number.js';
+
+const games = [
+  {
+    id: 1,
+    name: 'Високосний калькулятор',
+    category: 'numerical',
+    class: 'leap-year-calculator',
+  },
+  {
+    id: 2,
+    name: 'Вгадай число',
+    category: 'numerical',
+    class: 'guess-number',
+    init: guessNumberInit(),
+  },
+  {
+    id: 3,
+    name: 'Камінь-Ножиці-Папір',
+    category: 'game',
+    class: 'rock-paper-scissors',
+  },
+  {
+    id: 4,
+    name: 'Калькулятор',
+    category: 'numerical',
+    class: 'calculator',
+  },
+  {
+    id: 5,
+    name: 'Калькулятор часу',
+    category: 'numerical',
+    class: 'calculator-time',
+  },
+  {
+    id: 6,
+    name: 'Google динозаврик',
+    category: 'game',
+    class: 'google-din',
+  },
+  {
+    id: 7,
+    name: 'Футбол',
+    category: 'game',
+    class: 'soccer',
+  },
+  {
+    id: 8,
+    name: 'Найбільше число',
+    category: 'numerical',
+    class: 'biggest-number',
+  },
+  {
+    id: 9,
+    name: 'Наша команда',
+    category: 'acquaintance',
+    class: 'our-team',
+  },
+  {
+    id: 10,
+    name: 'Вчений',
+    category: 'acquaintance',
+    class: 'scientist',
+  },
+];
+
+export default games;
