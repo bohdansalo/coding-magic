b5f053a02c370157e1a40d98a027ea5a0bca3d4a
vovatrohymenko <157517964+vovatrohymenko@users.noreply.github.com>
2025-06-08 23:26:06 +0300

    Видалив не потрібні кольори

--- src/js/games/three-numbers.js
+++ src/js/games/three-numbers.js
@@ -1,5 +1,7 @@
 export const threeNumbersInit = () => {
   const gameContainer = document.querySelector('.biggest-number');
-  const gameHtml = `<h2 class="threenumbers">Введіть 3 числа</h2>`;
+  const gameHtml = `<h2 class="threenumbers">Введіть 3 числа</h2>
+ `;
+
   gameContainer.innerHTML = gameHtml;
 };
