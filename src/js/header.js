ef60d9e1e32c21462268c0cfe58f5332a3b07f6d
Богдан <bohdansalo31@gmail.com>
2025-06-08 19:07:39 +0300

    added padding header

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,18 @@
-const themeToggle = document.querySelector('[data-theme]');
-const darkMain = document.querySelector('[data-dark]');
+// const themeToggle = document.querySelector('[data-theme]');
+// const darkMain = document.querySelector('[data-dark]');
 
-themeToggle.addEventListener('change', () => {
-  document.body.classList.toggle('dark', themeToggle.checked);
-});
+// themeToggle.addEventListener('change', () => {
+//   document.body.classList.toggle('dark', themeToggle.checked);
+// });
+
+const themeSwiper = document.querySelector('[data-theme-swiper]');
+
+const handleTheme = event => {
+  if (event.target.checked) {
+    document.documentElement.dataset.theme = 'dark';
+  } else {
+    document.documentElement.dataset.theme = 'light';
+  }
+};
+
+themeSwiper.addEventListener('change', handleTheme);
