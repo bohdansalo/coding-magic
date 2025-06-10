beb76b30b022fb71331f6c198ec5f39881491abf
unknown <bieldanil240@gamil.com>
2025-06-10 10:40:59 +0200

    перемістив html в js

--- src/js/data/games.js
+++ src/js/data/games.js
@@ -1,5 +1,6 @@
 import { guessNumberInit } from '../games/guess-number.js';
 import { threeNumbersInit } from '../games/three-numbers.js';
+import { calculatorGame } from '../games/game-calculator.js';
 const games = [
   {
     id: 1,
@@ -25,6 +26,7 @@ const games = [
     name: 'Калькулятор',
     category: 'numerical',
     class: 'calculator',
+    init: calculatorGame,
   },
   {
     id: 5,
