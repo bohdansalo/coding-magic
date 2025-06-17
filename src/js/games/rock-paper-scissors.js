8ddc1418741451fa3bd6334d0cdb8a3665c53d12
Богдан <bohdansalo31@gmail.com>
2025-06-17 20:04:17 +0300

    completed js rsp game and added style

--- src/js/games/rock-paper-scissors.js
+++ src/js/games/rock-paper-scissors.js
@@ -1,3 +1,7 @@
+import stone from '../../images/main/game/stone.webp';
+import paper from '../../images/main/game/paper.webp';
+import scissors from '../../images/main/game/scissors.webp';
+
 export const rockScissorsPaperInit = () => {
   const container = document.querySelector('[data-game_container]');
   let gameContainer = container.querySelector('.guess-number');
@@ -7,17 +11,23 @@ export const rockScissorsPaperInit = () => {
     <h2 class="rsp__title">Камінь - ножиці - папір</h2>
 
     <div class="rsp__game-wrapper">
-      <ul class="rsp__list">
-        <li><button class="rsp__btn" data-rsp="rock">r</button></li>
-        <li><button class="rsp__btn" data-rsp="scissors">s</button></li>
-        <li><button class="rsp__btn" data-rsp="paper">p</button></li>
+      <ul class="rsp__list" data-rps-action>
+        <li><button class="rsp__btn" data-rsp="rock">
+         <img src= "${stone}" alt="stone"></img>
+        </button></li>
+        <li><button class="rsp__btn" data-rsp="scissors">
+         <img src= "${scissors}" alt="scissors"></img>
+        </button></li>
+        <li><button class="rsp__btn" data-rsp="paper">
+         <img src= "${paper}" alt="paper"></img>
+        </button></li>
       </ul>
 
       <div class="rsp__score">
         <h3 class="rsp__score-title">Рахунок:</h3>
         <ul class="rsp__score-list">
-          <li class="rsp__score-item">Комп'ютер - <span data-rsp-result="computer">0</span></li>
-          <li class="rsp__score-item">Ви - <span data-rsp-result="user">0</span></li>
+          <li class="rsp__score-item"><span data-rsp-result="computer">Комп'ютер - 0</span></li>
+          <li class="rsp__score-item"><span data-rsp-result="user">Ви - 0</span></li>
         </ul>
       </div>
     </div>
@@ -30,42 +40,50 @@ export const rockScissorsPaperInit = () => {
 
   const choices = ['rock', 'scissors', 'paper'];
 
-  const rspButtons = document.querySelectorAll('[data-rsp]');
-  const computerScore = document.querySelector('[data-rsp-result="computer"]');
-  const userScore = document.querySelector('[data-rsp-result="user"]');
+  const userChoiceButton = document.querySelector('[data-rps-action]');
+  const computerScoreOutput = document.querySelector(
+    '[data-rsp-result="computer"]'
+  );
+  const userScoreOutput = document.querySelector('[data-rsp-result="user"]');
   const winerLoser = document.querySelector('[data-win-lose]');
   const variantsComputer = document.querySelector('[data-rsp-start]');
+  let userScore = 0;
+  let computerScore = 0;
+  let userChoice = null;
 
-  rspButtons.forEach(button => {
-    button.addEventListener('click', e => {
-      const userSelection = e.target.dataset.rsp;
+  userChoiceButton.addEventListener('click', event => {
+    if (event.target.tagName === 'BUTTON') {
+      userChoice = event.target.dataset.rsp;
+    }
+  });
 
-      const randomChoice = Math.floor(Math.random() * 3);
-      const computerSelection = choices[randomChoice];
-      alert(`Комп'ютер обрав: ${computerSelection}`);
+  variantsComputer.addEventListener('click', e => {
+    const userSelection = userChoice;
 
-      if (userSelection === computerSelection) {
-        winerLoser.textContent = 'Нічия!';
-        winerLoser.style.color = 'var(--color-background-switch)';
+    const randomChoice = Math.floor(Math.random() * 3);
+    const computerSelection = choices[randomChoice];
 
-        return;
-      }
+    if (userSelection === computerSelection) {
+      winerLoser.textContent = 'Нічия!';
+      winerLoser.style.color = 'var(--color-background-switch)';
+      return;
+    }
 
-      const userWins =
-        (userSelection === 'rock' && computerSelection === 'scissors') ||
-        (userSelection === 'scissors' && computerSelection === 'paper') ||
-        (userSelection === 'paper' && computerSelection === 'rock');
+    const userWins =
+      (userSelection === 'rock' && computerSelection === 'scissors') ||
+      (userSelection === 'scissors' && computerSelection === 'paper') ||
+      (userSelection === 'paper' && computerSelection === 'rock');
 
-      if (userWins) {
-        userScore.textContent = parseInt(userScore.textContent || '0') + 1;
-        winerLoser.textContent = 'Ви виграли!';
-        winerLoser.style.color = 'var(--color-text-tretiary)';
-      } else {
-        computerScore.textContent =
-          parseInt(computerScore.textContent || '0') + 1;
-        winerLoser.textContent = 'Ви програли!';
-        winerLoser.style.color = 'var(--color-text-wrong)';
-      }
-    });
+    if (userWins) {
+      userScore += 1;
+      userScoreOutput.textContent = `Ви - ${userScore}`;
+      winerLoser.textContent = 'Ви виграли!';
+      winerLoser.style.color = 'var(--color-text-tretiary)';
+    } else {
+      computerScore += 1;
+      computerScoreOutput.textContent = `Комп'ютер - ${computerScore}`;
+      winerLoser.textContent = 'Ви програли!';
+      winerLoser.style.color = 'var(--color-text-wrong)';
+    }
   });
 };
