76b25ed0bb8e48b06d688b920381c38a7f41851b
unknown <bieldanil240@gamil.com>
2025-06-17 19:05:12 +0200

    new modal styles

--- src/js/games/footer-modal.js
+++ src/js/games/footer-modal.js
@@ -1,29 +1,18 @@
-const inputCheck = document.querySelector('.subscribe__input');
-const subscribeBtn = document.querySelector('.subscribe__button');
+const subscribeForm = document.querySelector('.subscribe__container');
 const modal = document.getElementById('thankYouModal');
 const closeModal = document.getElementById('closeModal');
 
-subscribeBtn.addEventListener('click', function () {
-  const email = inputCheck.value.trim();
-
-  if (email.endsWith('@gmail.com') && email.length >= 15) {
-    modal.classList.remove('footer__hidden');
-  } else {
-  }
+subscribeForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  modal.classList.remove('footer__hidden');
 });
 
 closeModal.addEventListener('click', function () {
   modal.classList.add('footer__hidden');
 });
 
-modal.addEventListener('click', function (e) {
-  if (e.target === modal) {
-    modal.classList.add('footer__hidden');
-  }
-});
-
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape') {
+  if (e.key === 'Escape' && !modal.classList.contains('.footer__hidden')) {
     modal.classList.add('footer__hidden');
   }
 });
