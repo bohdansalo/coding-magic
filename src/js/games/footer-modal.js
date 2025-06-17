1aae05da9b801d66558a24c56a57ca8475d9ab36
unknown <bieldanil240@gamil.com>
2025-06-17 14:16:18 +0200

    footer bug fixed

---
+++ src/js/games/footer-modal.js
@@ -0,0 +1,29 @@
+const inputCheck = document.querySelector('.subscribe__input');
+const subscribeBtn = document.querySelector('.subscribe__button');
+const modal = document.getElementById('thankYouModal');
+const closeModal = document.getElementById('closeModal');
+
+subscribeBtn.addEventListener('click', function () {
+  const email = inputCheck.value.trim();
+
+  if (email.endsWith('@gmail.com') && email.length >= 15) {
+    modal.classList.remove('footer__hidden');
+  } else {
+  }
+});
+
+closeModal.addEventListener('click', function () {
+  modal.classList.add('footer__hidden');
+});
+
+modal.addEventListener('click', function (e) {
+  if (e.target === modal) {
+    modal.classList.add('footer__hidden');
+  }
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    modal.classList.add('footer__hidden');
+  }
+});
