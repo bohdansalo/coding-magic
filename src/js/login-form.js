f47bc5c2fa7d9b390e21f6ed7027756a76b26efd
vovatrohymenko <157517964+vovatrohymenko@users.noreply.github.com>
2025-06-21 20:20:22 +0300

    доробив модальне вікно

--- src/js/login-form.js
+++ src/js/login-form.js
@@ -1,22 +1,23 @@
-'use strict';
 document.addEventListener('DOMContentLoaded', () => {
   const backdrop = document.querySelector('.backdrop');
   const closeBtn = document.querySelector('.login-form-clos');
   const form = document.querySelector('.login-form');
+  backdrop.classList.remove('hidden');
   closeBtn.addEventListener('click', () => {
     backdrop.classList.add('hidden');
   });
-  form.addEventListener('submit', event => {
-    event.preventDefault();
-    backdrop.classList.add('hidden');
-  });
-  document.addEventListener('keydown', event => {
-    if (event.key === 'Escape') {
+  document.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
       backdrop.classList.add('hidden');
     }
   });
-});
-const closeBtn = document.querySelector('.login-form-clos');
-closeBtn.addEventListener('click', () => {
-  backdrop.classList.add('hidden');
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    backdrop.classList.add('hidden');
+    const name = document.querySelector('#name').value;
+    const nameDisplay = document.querySelector('.login-form-user');
+    if (name && nameDisplay) {
+      nameDisplay.textContent = name;
+    }
+  });
 });
