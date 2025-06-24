325d3db9693736956061ce0b44f9337b7ea376b1
vovatrohymenko <157517964+vovatrohymenko@users.noreply.github.com>
2025-06-24 20:59:38 +0300

    .

--- src/js/games/game-leap-year-calculator.js
+++ src/js/games/game-leap-year-calculator.js
@@ -1,18 +1,17 @@
 export const leapYearCalculatorInit = () => {
   const gameContainer = document.querySelector('.leap-year-calculator');
-  const gameHtml = ` 
-<section class="year-calculator">
-      <div class="container">
-        <h2 class="year-calculator-title">
-         Перевір в який рік ти народився
-         </h2>
-         <input
-         class="year-calculator-input"
-          type="number"
-         placeholder="Введіть рік народження" />
-        <button class="year-calculator-button"></button>
-        <span class="year-calculator-result"></span>
-         </div>
-</section>`;
+  const gameHtml = `
+  <h2 class="year-calculator-title">Перевір в який рік ти народився</h2>
+          <form class="year-calculator-content">
+            <input
+              class="year-calculator-input"
+              type="number"
+              placeholder="Введіть рік народження"
+            />
+            <button class="year-calculator-button"></button>
+            </form>
+            <span class="year-calculator-result"></span>
+          
+`;
   gameContainer.innerHTML = gameHtml;
 };
