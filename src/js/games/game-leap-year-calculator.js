8c5e01ea95b6f42144755d2f767d7a52b84646e2
vovatrohymenko <157517964+vovatrohymenko@users.noreply.github.com>
2025-06-30 20:25:20 +0300

    Високосний калькулятор

--- src/js/games/game-leap-year-calculator.js
+++ src/js/games/game-leap-year-calculator.js
@@ -1,7 +1,9 @@
 export const leapYearCalculatorInit = () => {
   const gameContainer = document.querySelector('.leap-year-calculator');
   const gameHtml = `
-  <h2 class="year-calculator-title">Перевір в який рік ти народився</h2>
+    <div class="leap-year-calculator">
+        <h2 class="year-calculator-title">Перевір в який рік ти народився</h2>
+        <div class="year-calculator-wrapper">
           <form class="year-calculator-content">
             <input
               class="year-calculator-input"
@@ -9,9 +11,34 @@ export const leapYearCalculatorInit = () => {
               placeholder="Введіть рік народження"
             />
             <button class="year-calculator-button"></button>
-            </form>
-            <span class="year-calculator-result"></span>
-          
+          </form>
+          <span class="year-calculator-result">Ви народилися у високосний рік!</span>
+        </div>
+      </div>    
 `;
+  const form = gameContainer.querySelector('.year-calculator-content');
+  const input = gameContainer.querySelector('.year-calculator-input');
+  const result = gameContainer.querySelector('.year-calculator-result');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    const year = parseInt(input.value.trim());
+
+    if (!year || year < 0) {
+      result.textContent = 'Введіть коректний рік!';
+      result.style.color = 'orange';
+      return;
+    }
+
+    const isLeap = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+
+    if (isLeap) {
+      result.textContent = 'Ви народилися у високосний рік!';
+      result.style.color = 'green';
+    } else {
+      result.textContent = 'Ви народилися не у високосний рік!';
+      result.style.color = 'red';
+    }
+  });
   gameContainer.innerHTML = gameHtml;
 };
